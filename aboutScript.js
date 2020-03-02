51e9afa0b2d9627636b6aca409ac339b064d4b82
muse-gabs <45804707+muse-gabs@users.noreply.github.com>
2020-03-01 17:00:55 -0700

    Create aboutScript.js

---
+++ aboutScript.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+   url = 'https://script.google.com/macros/s/AKfycbzjhVtdQBOd6vuWGPNG_avUgl0xehLyPzxbPKokGE8w-w_evNY/exec'
+   
+$('#submit-form').on('click', function(e) {
+   e.preventDefault();
+   var jqxhr = $.ajax({
+      url: url,
+      method: "GET",
+      dataType: "json",
+      data: $form.serializeObject()
+      }).success(
+         
+      );
+})
